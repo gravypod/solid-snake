2696f64d4c0b92f29ed89449217d945332b7abac
Joshua Katz <gravypod@gravypod.com>
2017-09-26 12:35:48 -0400

    Clean up rotation code.

--- src/graphics/style.c
+++ src/graphics/style.c
@@ -53,8 +53,9 @@ void draw_style(const char const *name, float x, float y, float rotation)
     static bool has_initialized = false;
     static mat4x4 translation, projection;
 
-    struct style *s = llist_get(&styles, name);
-
+    const struct style *s = llist_get(&styles, name);
+    const quadmesh m = s->m;
+    const shader_program  program = s->program;
 
     if (!has_initialized) {
         mat4x4_ortho(projection, 0, 1, 0, 1, 0, 1);
@@ -65,15 +66,23 @@ void draw_style(const char const *name, float x, float y, float rotation)
     mat4x4_translate(translation, x, y, 0);
 
     if (rotation != 0) {
-        // Rotate around center.
-        mat4x4_translate_in_place(translation, (s->m.width / 2), (s->m.height / 2), 0);
+        // Find center location of the style
+        const float center_x = m.width / 2;
+        const float center_y = m.height / 2;;
+
+        // Move to the center of the style
+        mat4x4_translate_in_place(translation,  center_x,  center_y, 0);
+
+        // Rotate from center of the style
         mat4x4_rotate_Z(translation, translation, rotation);
-        mat4x4_translate_in_place(translation, -(s->m.width / 2), -(s->m.height / 2), 0);
+
+        // Move back into 0,0 of our style
+        mat4x4_translate_in_place(translation, -center_x, -center_y, 0);
     }
 
-    USE_SHADER(s->program, {
-        const GLint translation_matrix_location = glGetUniformLocation(s->program, "translation");
-        const GLint  projection_matrix_location = glGetUniformLocation(s->program,  "projection");
+    USE_SHADER(program, {
+        const GLint translation_matrix_location = glGetUniformLocation(program, "translation");
+        const GLint  projection_matrix_location = glGetUniformLocation(program,  "projection");
 
         glUniformMatrix4fv(translation_matrix_location, 1, GL_FALSE, (GLfloat*) translation);
         glUniformMatrix4fv( projection_matrix_location, 1, GL_FALSE, (GLfloat*) projection);
@@ -91,11 +100,11 @@ void draw_style(const char const *name, float x, float y, float rotation)
         const GLint width  = glGetUniformLocation(s->program, "quad_width");
         const GLint height = glGetUniformLocation(s->program, "quad_height");
         if (width >= 1 && height >= 1) {
-            glUniform1f(width, s->m.width);
-            glUniform1f(height, s->m.height);
+            glUniform1f(width, m.width);
+            glUniform1f(height, m.height);
         }
 
-        draw_quadmesh(&s->m);
+        draw_quadmesh(&m);
 
     });
 
